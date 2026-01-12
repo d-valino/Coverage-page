4cd65ed86a4e6c42e17b5c1d478176a8ab8b24c8
Daniel Valino Ovalle <valinoovalledanieljesus@gmail.com>
2026-01-12 16:50:58 +0100

    [ADD] setup Vue with Vite

---
+++ coverage-page/src/main.ts
@@ -0,0 +1,7 @@
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
+
+createApp(App).mount('#app')
+// Use App.vue as root component
+// Put the component in <div id="app"></div>
