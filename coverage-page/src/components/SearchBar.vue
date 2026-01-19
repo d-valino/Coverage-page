cfe77901a8422c4662bd80584ef1abe40fa398d3
Daniel Valino Ovalle <valinoovalledanieljesus@gmail.com>
2026-01-19 01:53:38 +0100

    [ADD] clear button in search bar

--- coverage-page/src/components/SearchBar.vue
+++ coverage-page/src/components/SearchBar.vue
@@ -14,19 +14,35 @@ import { computed } from 'vue';
 		set: value => emit('update:search', value)
 	})
 
+	const clearSearch = () => {
+		emits.value = ''
+	}
 </script>
+
 
 <template>
 	<div class="searchFilter">
-		<input
-			class="searchBar"
-			v-model="emits"
-			type="text"
-			placeholder="Search Platform..."
-			aria-label="Search platform by name"
-		>
+		<div class="searchWrapper">
+			<input
+				class="searchBar"
+				v-model="emits"
+				type="text"
+				placeholder="Search Platform..."
+				aria-label="Search platform by name"
+			>
+			<button
+				v-if="emits"
+				@click="clearSearch"
+				type="button"
+				aria-label="Clear search"
+				class="clearButton"
+			>
+			✕
+			</button>
+		</div>
 	</div>
 </template>
+
 
 <style scoped>
 	.searchBar {
@@ -38,10 +54,35 @@ import { computed } from 'vue';
 		border: 0.5px solid lightgrey;
 		padding-right: 40px;
 		padding-left: 10px;
+		box-sizing: border-box;
 	}
 	.searchFilter {
 		flex: 1;
 		display: flex;
 		justify-content: flex-end;
+		width: 100%;
+		box-sizing: border-box;
+		max-width: 100%;
+		padding: 0 10px;
+	}
+	.searchWrapper {
+		position: relative;
+		width: 100%;
+		max-width: 500px;
+	}
+	.clearButton {
+		position: absolute;
+		color: #b0b0b0;
+		cursor: pointer;
+		font-family: inherit;
+		right: 5px;
+		top: 50%;
+		transform: translateY(-50%);
+		background: transparent;
+		border: none;
+		padding: 0;
+	}
+	.clearButton:hover {
+		color: black;
 	}
 </style>
